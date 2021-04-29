5955d3f3fe4bd6f1b9ee6a3ee93827be4694037c
Yana <yana.sitkowecz@yandex.ru>
2021-04-29 15:14:22 +0300

    completed

--- Lab10/Lab10/Program.cs
+++ Lab10/Lab10/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.IO;
+using System.Collections;
+using System.Linq;
 
 
 namespace Lab10
@@ -9,9 +11,18 @@ namespace Lab10
         public static void Main(string[] args)
         {
             StreamReader src = new StreamReader("Inlet.in");
-            string[] numbers = src.ReadLine().Split(' ');
-            foreach (var item in numbers)
-                Console.WriteLine();
+            StreamWriter csr = new StreamWriter("Outlet.out");
+            int[] numbers = src.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(x => int.Parse(x)).ToArray();
+            IEnumerator ie = numbers.GetEnumerator();
+            while (ie.MoveNext())
+            {
+                var item = (int)ie.Current;
+                if ((item / 100000) % 10 + (item / 10000) % 10 + ((item / 1000) % 100) % 10 == (item % 1000) / 100 + ((item % 1000) % 100) / 10 + ((item % 1000) % 100) % 10)
+                    csr.WriteLine(item);
+            }
+            csr.Close();
+
+
         }
     }
 }
